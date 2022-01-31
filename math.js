cb5a06e5973b8199b9e7d3192ef11cdd907d0483
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-31 23:41:35 +0600

    math concept clear

---
+++ math.js
@@ -0,0 +1,33 @@
+// Math.abs 
+// var num = -5;
+// var output = Math.abs(num);
+// console.log(output);
+
+// Math.ceil 
+// var number = 33.33;
+// var output = Math.ceil(number)
+// console.log(output);
+
+// Math.floor 
+// var number = 33.33;
+// var output = Math.floor(number)
+// console.log(output);
+
+// Math.round 
+// var number = 33.33;
+// var output = Math.round(number)
+// console.log(output);
+
+// Math.random
+
+// var number = 33.33;
+// var output = Math.random()*6;
+// var rounded = Math.round(output);
+// console.log(rounded);
+
+for(var num =0; num <= 30; num++){
+    var output = Math.random()*6;
+    var rounded = Math.round(output);
+        console.log(rounded);
+    
+}
